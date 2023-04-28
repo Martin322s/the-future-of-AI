32a33d5a1eebb6fc78d8d8aa8d9b359a81b76f0f
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-28 23:17:29 +0300

    home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Headers";
+import Home from "./components/Home/Home";
 
 function App() {
     return (
         <>
             <Header />
+            <Home />
         </>
     );
 }
