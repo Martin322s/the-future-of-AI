678529222a4f035f229259b41fb6148e3148c9e7
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-28 23:44:05 +0300

    home page title

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,12 @@ import logo from "./images/handshake.png";
 
 const Home = () => {
     return (
-        <img className={`${styles["backgr-img"]}`}src={logo} alt="background" />
+        <>
+            <img className={`${styles["backgr-img"]}`} src={logo} alt="background" />
+            <h1 className={`${styles["home-heading"]}`}>Artificial Intelligence</h1>
+            <p className={`${styles["home-subheading"]}`}>Stay Up-to-Date with the Latest AI News and Developments</p>
+            <button className={`${styles["home-btn"]}`}>Catalog news</button>
+        </>
     );
 };
 
